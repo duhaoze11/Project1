c59567bbfca938c592fb0ee84e8638f86ccd4f15
杜昊泽 <dujinghao11@hotmail.com>
2016-02-04 12:48:52 +0800

    add

---
+++ Project1/CodeFile1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace a_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            try
+            {
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("The input are not numbers");
+            }
+            Console.WriteLine("a+b="+( a + b));
+            Console.Read();
+        }
+    }
+}
